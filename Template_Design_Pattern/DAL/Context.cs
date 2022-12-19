6c9d88b1a2f14b548f0f22b66f0c1463154351b0
ulusoydev <eulusoy139@gmail.com>
2022-12-19 21:58:07 +0300

    Add project files.

---
+++ Template_Design_Pattern/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Template_Design_Pattern.DAL.Entities;
+
+namespace Template_Design_Pattern.DAL
+{
+    public class Context:IdentityDbContext<AppUser, AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-0A6CNA1;initial catalog=DbTemplateDesingPatten; integrated security=true");
+        }
+    }
+}
---
+++ Template_Design_Pattern/TemplateDesignPattern/UserCardTemplate.cs
@@ -0,0 +1,9 @@
+﻿using Template_Design_Pattern.DAL.Entities;
+
+namespace Template_Design_Pattern.TemplateDesignPattern
+{
+    public abstract class UserCardTemplate
+    {
+        protected AppUser AppUser { get; set; }
+    }
+}
